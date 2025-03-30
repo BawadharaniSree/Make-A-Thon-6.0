3b73c9eab90c1271d2f71d3c0510959b89ec6a34
Akshay19072004 <akshaygangadharan004@gmail.com>
2025-03-30 18:34:21 +0530

    Update floating-date-badge.tsx

--- components/floating-date-badge.tsx
+++ components/floating-date-badge.tsx
@@ -22,7 +22,7 @@ export default function FloatingDateBadge() {
           </div>
           <div className="text-purple-300 text-xs font-medium mb-1">Phase 2 PS Released</div>
           <div className="flex items-center gap-1 text-gray-400 text-xs">
-            <Clock className="h-3 w-3" />
+{/*             <Clock className="h-3 w-3" /> */}
             <span>Check Out!!!</span>
           </div>
 
