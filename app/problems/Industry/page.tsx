603389a2da823ae53f313c23444132f8d5711411
Akshay19072004 <akshaygangadharan004@gmail.com>
2025-03-30 18:57:03 +0530

    Update page.tsx

--- app/problems/Industry/page.tsx
+++ app/problems/Industry/page.tsx
@@ -60,15 +60,15 @@ export default function MiscPage() {
       "sdg": "SDG 9: Industry, Innovation, and Infrastructure",
       "fullDescription": "Implement a communication protocol for satellites to share information and data continuously between satellite and ground stations. This protocol should prioritize data security and reliability in transmission."
     },
-    {
-      "id": 55,
-      "title": "Space Debris Tracking and Mitigation: ",
-      "description": "Develop methods for tracking and mitigating space debris using optical sensors and specialized algorithms to prevent collisions and ensure space sustainability.",
-      "difficulty": "Hard",
-      "tags": ["Aerospace", "Space Technology", "AI & Computer Vision"],
-      "sdg": "SDG 9: Industry, Innovation, and Infrastructure",
-      "fullDescription": "Create innovative approaches for monitoring space debris using Optical sensors, complemented by specialized algorithms. The focus should be on strategies for collision avoidance and the removal of debris."
-    }
+    // {
+    //   "id": 55,
+    //   "title": "Space Debris Tracking and Mitigation: ",
+    //   "description": "Develop methods for tracking and mitigating space debris using optical sensors and specialized algorithms to prevent collisions and ensure space sustainability.",
+    //   "difficulty": "Hard",
+    //   "tags": ["Aerospace", "Space Technology", "AI & Computer Vision"],
+    //   "sdg": "SDG 9: Industry, Innovation, and Infrastructure",
+    //   "fullDescription": "Create innovative approaches for monitoring space debris using Optical sensors, complemented by specialized algorithms. The focus should be on strategies for collision avoidance and the removal of debris."
+    // }
   ]
 
   return (
